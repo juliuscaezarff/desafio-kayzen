6814fb102cc2851eb2463be736718d325f9a11d9
julius caezar <julius.caezar25@gmail.com>
2023-08-15 23:17:20 -0400

    refactor: refatorando

--- src/components/Organisms/HomeFormSection/index.tsx
+++ src/components/Organisms/HomeFormSection/index.tsx
@@ -1,48 +1,21 @@
 // Styles
-import Image from 'next/image'
 import './styles.css'
 
-// Assets
+// components
+import HomeFormComponent from '@/components/Atoms/HomeFormComponent'
 
 const HomeFormSection = () => {
   return (
     <section className="home-form-section">
-      <h1 className="text-4xl font-semibold text-white max-w-[303px]">
+      <h1 className="title-form-container">
         Vem ser nosso parceiro, entre em contato.
       </h1>
-      <form className="w-full max-w-[360px] flex flex-col gap-4 mt-[60px] ">
-          <input
-            className="h-[59px] p-[20px]"
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Nome"
-          />
-          <input
-            className="h-[59px]  p-[20px]"
-            type="text"
-            name="phone"
-            id="phone"
-            placeholder="Telefone"
-          />
-          <input
-            className="h-[59px]  p-[20px]"
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Email"
-          />
-          <textarea
-          className='h-[131px]  p-[20px]'
-            name="message"
-            id="message"
-            placeholder="Mensagem"
-          ></textarea>
-      </form>
+
+      <HomeFormComponent />
 
       <button className="px-16 mt-[64px] bg-blue-primary text-white rounded-3xl h-[53px] uppercase shadow-lg shadow-gray-700">
-          Enviar Mensagem
-        </button>
+        Enviar Mensagem
+      </button>
     </section>
   )
 }

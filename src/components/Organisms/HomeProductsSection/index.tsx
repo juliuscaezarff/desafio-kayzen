870838c40743f8b68c4b9288aaf78d4d5ba87047
julius caezar <julius.caezar25@gmail.com>
2023-08-15 22:38:24 -0400

    refactor: refatorando

--- src/components/Organisms/HomeProductsSection/index.tsx
+++ src/components/Organisms/HomeProductsSection/index.tsx
@@ -1,31 +1,41 @@
 // Styles
-import Image from 'next/image'
 import './styles.css'
 
-// Assets
-import ProductsMobile from '../../../../public/assets/via-certa-home-products-mobile.png'
-import BrandsOne from '../../../../public/logos/foto-pagina-home-brands-one-mobile.svg'
-import BrandsTwo from '../../../../public/logos/foto-pagina-home-brands-two-mobile.svg'
+//next
+import Image from 'next/image'
 
 const HomeProductsSection = () => {
   return (
     <section className="home-products-section">
-      <h1 className="text-4xl font-semibold text-blue-primary-light max-w-[333px]">
+      <h1 className="title-container">
         Conheça as Marcas que estão com a gente
       </h1>
-      <p className="text-base font-normal text-sub-title-dark max-w-[333px] mt-[20px]">
+      <p className="sub-title-container">
         A Via Certa traz variedade, com produtos de diversos segmentos
         diferentes:
       </p>
-      <div className="w-full h-[50px] bg-red-500 mt-[60px] text-center">
-        fdf
-      </div>
+      <div className="carrousel-container">fdf</div>
       <div className="bg-blue-light py-[19px] px-[35px]">
-        <Image src={ProductsMobile} alt="imagem" />
+        <Image
+          src="/assets/via-certa-home-products-mobile.png"
+          alt="imagem"
+          width={321}
+          height={813}
+        />
       </div>
-      <div className="bg-blue-primary-dark flex py-[60px] px-[45px]">
-        <Image src={BrandsOne} alt="imagem" />
-        <Image src={BrandsTwo} alt="imagem" height={173} />
+      <div className="brands-container">
+        <Image
+          src="/logos/foto-pagina-home-brands-one-mobile.svg"
+          alt="imagem"
+          width={150}
+          height={195}
+        />
+        <Image
+          src="/logos/foto-pagina-home-brands-two-mobile.svg"
+          alt="imagem"
+          width={150}
+          height={173}
+        />
       </div>
     </section>
   )

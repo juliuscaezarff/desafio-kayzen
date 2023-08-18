69fe53f5b09e6bc1cdfeaa45d9f2688d8fe3e01e
julius caezar <julius.caezar25@gmail.com>
2023-08-17 20:50:26 -0400

    feat: criando o template da página de contato

---
+++ src/components/Templates/ContactPage/index.tsx
@@ -0,0 +1,15 @@
+// Components
+import ContactIntroSection from "@/components/Organisms/ContactIntroSection"
+import ContactInformationSection from "@/components/Organisms/ContactInformationSection"
+
+
+function ContactPage() {
+  return (
+    <>
+     <ContactIntroSection />
+     <ContactInformationSection />
+    </>
+  )
+}
+
+export default ContactPage

b8509b237f7fc9aaf7b21defde259af47200dc80
julius caezar <julius.caezar25@gmail.com>
2023-08-18 22:45:55 -0400

    feat: criando o template para a página blog

---
+++ src/components/Templates/BlogPage/index.tsx
@@ -0,0 +1,13 @@
+// Components
+import BlogPublicationSection from "@/components/Organisms/BlogPublicationsSection"
+
+
+function BlogPage() {
+  return (
+    <>
+     <BlogPublicationSection />
+    </>
+  )
+}
+
+export default BlogPage

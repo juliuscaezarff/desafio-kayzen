9b7a789c723ed88f4e1dccb8e6ac551a8086b4e2
julius caezar <julius.caezar25@gmail.com>
2023-08-18 22:44:02 -0400

    feat: criando o component para as publicações na página blog

---
+++ src/components/Atoms/BlogAnotherPublicationsComponent/index.tsx
@@ -0,0 +1,43 @@
+const BlogAnotherPublicationsComponent = () => {
+  return (
+    <div className="hidden md:block">
+            <div className="mt-[40px]">
+              <p className="font-normal text-base text-gray-400">10 Out 2023</p>
+              <p className="font-normal text-sm max-w-[329px]">
+                Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit
+                amet, consectetur Lorem ipsum dolor sit amet, consectetur Lorem
+                ipsum dolor sit amet, consectetur Lorem ipsum...
+              </p>
+              <p className="font-semibold text-base text-red-500 mt-[20px] text-right underline">
+                Ler mais
+              </p>
+            </div>
+
+            <div className="">
+              <p className="font-normal text-base text-gray-400">10 Out 2023</p>
+              <p className="font-normal text-sm max-w-[329px]">
+                Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit
+                amet, consectetur Lorem ipsum dolor sit amet, consectetur Lorem
+                ipsum dolor sit amet, consectetur Lorem ipsum...
+              </p>
+              <p className="font-semibold text-base text-red-500 mt-[20px] text-right underline">
+                Ler mais
+              </p>
+            </div>
+
+            <div className="">
+              <p className="font-normal text-base text-gray-400">10 Out 2023</p>
+              <p className="font-normal text-sm max-w-[329px]">
+                Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit
+                amet, consectetur Lorem ipsum dolor sit amet, consectetur Lorem
+                ipsum dolor sit amet, consectetur Lorem ipsum...
+              </p>
+              <p className="font-semibold text-base text-red-500 mt-[20px] text-right underline">
+                Ler mais
+              </p>
+            </div>
+          </div>
+  )
+}
+
+export default BlogAnotherPublicationsComponent

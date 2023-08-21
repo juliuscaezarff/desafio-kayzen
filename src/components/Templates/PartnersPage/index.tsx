933ab3592345d751cc430f469a3d34c7b047a297
julius caezar <julius.caezar25@gmail.com>
2023-08-21 13:21:59 -0400

    feat: criando a página de 'nossos parceiros'

---
+++ src/components/Templates/PartnersPage/index.tsx
@@ -0,0 +1,12 @@
+// Components
+import PartnersSection from "@/components/Organisms/PartnersSection"
+
+function PartnersPage() {
+  return (
+    <>
+     <PartnersSection />
+    </>
+  )
+}
+
+export default PartnersPage

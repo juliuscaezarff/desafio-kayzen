e9fbf740693bc303cfb6b9ece2987ed09b301b23
julius caezar <julius.caezar25@gmail.com>
2023-08-21 13:30:45 -0400

    feat: criando a caixa de parceiros dinamicamente

---
+++ src/components/Molecules/PartnersBox/index.tsx
@@ -0,0 +1,17 @@
+interface Partner {
+  id: number;
+  imageSrc: string;
+  text1: string;
+  text2: string;
+  label: string;
+}
+
+export default function PartnersBox({ imageSrc, text1, text2 }: Partner) {
+  return (
+    <div className="w-[159px] h-[318px] flex flex-col gap-[33px] items-center md:w-[200px] md:h-[381px]">
+      <img src={imageSrc} alt="Imagem da fruta" />
+      <p className="font-semibold text-base text-blue-primary">{text1}</p>
+      <p className="font-medium text-sm text-dark">{text2}</p>
+    </div>
+  );
+}

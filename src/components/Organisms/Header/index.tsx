db36ca0364c98e0053baca076e748a7f5ceb9851
julius caezar <julius.caezar25@gmail.com>
2023-08-21 13:26:21 -0400

    fix: ajeitando os espaçametnos laterais

--- src/components/Organisms/Header/index.tsx
+++ src/components/Organisms/Header/index.tsx
@@ -17,14 +17,13 @@ import Image from 'next/image'
 
 const Header = () => {
   const [open, setOpen] = useState(false)
-  const [dropdownOpen, setDropdownOpen] = useState(false)
 
   const handleOpenMenu = () => {
     setOpen(!open)
   }
 
   return (
-    <header className="flex justify-between items-center p-3 lg:px-[102px] lg:py-7 lg:max-w-[1440px] lg:mx-auto">
+    <header className="flex justify-between items-center p-3 lg:px-[60px] lg:py-7 lg:max-w-[1440px] lg:mx-auto lg:bg-red-200">
       <Link
         href="/"
         className="hover:scale-110 hover:duration-200 w-[62px] h-[44px]"
@@ -35,29 +34,21 @@ const Header = () => {
 
       <ul className="hidden lg:flex lg:justify-center lg:items-center lg: lg:gap-6">
         <li className="relative">
-          <button type="button" className="link">
-            <span>Inicio</span>
-          </button>
+          <Link href={menu_navbar[0].href}>{menu_navbar[0].label}</Link>
         </li>
         <li>
-          <Link href={menu_navbar[1].href} className="link">
-            {menu_navbar[1].label}
-          </Link>
+          <Link href={menu_navbar[1].href}>{menu_navbar[1].label}</Link>
         </li>
         <li>
-          <Link href={menu_navbar[2].href} className="link">
-            {menu_navbar[2].label}
-          </Link>
+          <Link href={menu_navbar[2].href}>{menu_navbar[2].label}</Link>
         </li>
         <li>
-          <Link href={menu_navbar[3].href} className="link">
-            {menu_navbar[3].label}
-          </Link>
+          <Link href={menu_navbar[3].href}>{menu_navbar[3].label}</Link>
         </li>
       </ul>
 
       <a
-        href="/formulario-de-contato"
+        href="/"
         className="hidden bg-gradient-to-r from-red-500 via-red-600 to-red-700 text-white rounded-3xl lg:block lg: text-xs lg:font-bold lg:px-4 lg:py-3 lg:tracking-wide lg:shadow-gray-600"
       >
         FALAR COM A VIA CERTA
@@ -71,5 +62,3 @@ const Header = () => {
 }
 
 export default Header
-
-// #C70F08 #FF0000  #F52C2Cv box-shadow: 0px 4px 4px 0px #00000040;

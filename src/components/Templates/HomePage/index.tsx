c819a3b849482e368b6a6df366a5ac328d4cc5a3
julius caezar <julius.caezar25@gmail.com>
2023-08-21 22:02:18 -0400

    feat: removendo uma section

--- src/components/Templates/HomePage/index.tsx
+++ src/components/Templates/HomePage/index.tsx
@@ -2,9 +2,8 @@
 import HomeIntroSection from '@/components/Organisms/HomeIntroSection'
 import HomeProductsSection from '@/components/Organisms/HomeProductsSection'
 import HomeAchievementsSection from '@/components/Organisms/HomeAchievementsSection'
-import HomeBenefitsSection from '@/components/Organisms/HomeBenefitsSection'
+// import HomeBenefitsSection from '@/components/Organisms/HomeBenefitsSection'
 import HomeInformationSection from '@/components/Organisms/HomeInformationSection'
-import HomeFormSection from '@/components/Organisms/HomeFormSection'
 
 function AboutPage() {
   return (
@@ -12,7 +11,6 @@ function AboutPage() {
       <HomeIntroSection />
       <HomeProductsSection />
       <HomeAchievementsSection />
-      <HomeBenefitsSection />
       <HomeInformationSection />
     </>
   )

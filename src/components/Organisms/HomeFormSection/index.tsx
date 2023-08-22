2fe8b649a5fa21d015402d33eaf8153c4d568f6b
julius caezar <julius.caezar25@gmail.com>
2023-08-21 21:55:55 -0400

    feat: criando a versão desktop

--- src/components/Organisms/HomeFormSection/index.tsx
+++ src/components/Organisms/HomeFormSection/index.tsx
@@ -1,21 +1,20 @@
-// Styles
 import './styles.css'
-
-// components
 import HomeFormComponent from '@/components/Atoms/HomeFormComponent'
 
 const HomeFormSection = () => {
   return (
-    <section className="home-form-section">
-      <h1 className="title-form-container">
-        Vem ser nosso parceiro, entre em contato.
-      </h1>
+    <section className="home-form-section bg-no-repeat bg-center bg-cover md:bg-form-background">
+      <div className="flex flex-col items-center justify-center h-[882px] w-full bg-red-700 md:h-[715px] md:w-[767px]">
+        <h1 className="title-form-container">
+          Vem ser nosso parceiro, entre em contato.
+        </h1>
 
-      <HomeFormComponent />
+        <HomeFormComponent />
 
-      <button className="px-16 mt-[64px] bg-blue-primary text-white rounded-3xl h-[53px] uppercase shadow-lg shadow-gray-700">
-        Enviar Mensagem
-      </button>
+        <button className="px-16 mt-[64px] bg-blue-primary text-white rounded-3xl h-[53px] uppercase shadow-lg shadow-gray-700">
+          Enviar Mensagem
+        </button>
+      </div>
     </section>
   )
 }

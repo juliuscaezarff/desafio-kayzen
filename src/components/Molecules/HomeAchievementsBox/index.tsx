f9ec8ddfabce2ecd3dd59b85ee5a86a4c749d078
julius caezar <julius.caezar25@gmail.com>
2023-08-21 21:49:20 -0400

    feat: criando a versão desktop

--- src/components/Molecules/HomeAchievementsBox/index.tsx
+++ src/components/Molecules/HomeAchievementsBox/index.tsx
@@ -13,7 +13,7 @@ const HomeAchievementsBox = ({
   iconSrc
 }: AchievementsBoxProps) => {
   return (
-    <div className="w-full h-[135px] max-w-[340px] mx-auto mt-[56px] bg-red-200 flex flex-col items-center justify-center shadow-lg shadow-gray-300">
+    <div className="w-full h-[135px] max-w-[340px] mx-auto mt-[56px] flex flex-col items-center justify-center shadow-lg shadow-gray-300 md:max-w-[231px]">
       <div className="w-[50px] h-[50px] bg-blue-primary-light rounded-[25px] mt-[-50px] flex items-center justify-center">
         <Image src={iconSrc} alt="icone" width={25} height={20} />
       </div>

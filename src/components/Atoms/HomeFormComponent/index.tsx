23f691223e1a5709cda60cc6e70be81a5c9c0ac9
julius caezar <julius.caezar25@gmail.com>
2023-08-24 12:13:51 -0400

    fix: corrigindo o layout do formulario

--- src/components/Atoms/HomeFormComponent/index.tsx
+++ src/components/Atoms/HomeFormComponent/index.tsx
@@ -1,6 +1,6 @@
 const HomeFormComponent = () => {
   return (
-    <form className="w-[380px] flex flex-col gap-4 bg-red-700 md:w-[593px] ">
+    <form className="w-full flex flex-col gap-4 bg-red-700 md:w-[593px] px-4">
       <div className="flex flex-col justify-between gap-4 md:flex-row md:gap-[10px]">
       <input
         className="h-[59px] p-[20px] md:w-[286px]"

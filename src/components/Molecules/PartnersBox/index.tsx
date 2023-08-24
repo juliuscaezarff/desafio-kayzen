fd24cbcabf8e20e4ff08b66908f49c6eb8de2a9f
julius caezar <julius.caezar25@gmail.com>
2023-08-24 12:15:38 -0400

    feat: trocando a tag img por Image do next

--- src/components/Molecules/PartnersBox/index.tsx
+++ src/components/Molecules/PartnersBox/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 interface Partner {
   id: number;
   imageSrc: string;
@@ -9,7 +10,7 @@ interface Partner {
 export default function PartnersBox({ imageSrc, text1, text2 }: Partner) {
   return (
     <div className="w-[159px] h-[318px] flex flex-col gap-[33px] items-center md:w-[200px] md:h-[381px]">
-      <img src={imageSrc} alt="Imagem da fruta" />
+      <Image src={imageSrc} alt="Logo" width={180} height={180}/>
       <p className="font-semibold text-base text-blue-primary">{text1}</p>
       <p className="font-medium text-sm text-dark">{text2}</p>
     </div>

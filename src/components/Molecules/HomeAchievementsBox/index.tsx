2d4c8f11e488f44235bddfa7a120254787ce3ad2
julius caezar <julius.caezar25@gmail.com>
2023-08-24 15:47:23 -0400

    feat: adicionando a llib countup e criando mais 2 props

--- src/components/Molecules/HomeAchievementsBox/index.tsx
+++ src/components/Molecules/HomeAchievementsBox/index.tsx
@@ -1,25 +1,46 @@
+'use client'
+
 // Nextjs
 import Image from 'next/image'
 
+// Packages
+import CountUp from 'react-countup'
+import VisibilitySensor from 'react-visibility-sensor'
+
 interface AchievementsBoxProps {
   title: string
-  years: string
+  years: number
+  text?: string
+  plus?: string
   iconSrc: string
 }
 
 const HomeAchievementsBox = ({
   title,
   years,
+  text,
+  plus,
   iconSrc
 }: AchievementsBoxProps) => {
   return (
-    <div className="w-full h-[135px] max-w-[340px] mx-auto mt-[56px] flex flex-col items-center justify-center shadow-lg shadow-gray-300 md:max-w-[231px] bg-white">
-      <div className="w-[50px] h-[50px] bg-blue-primary-light rounded-[25px] mt-[-50px] flex items-center justify-center">
-        <Image src={iconSrc} alt="icone" width={25} height={20} />
-      </div>
-      <h1 className="font-extrabold text-[40px] text-red-primary">{years}</h1>
-      <p className="font-extrabold text-sm text-gray-dark">{title}</p>
-    </div>
+    <VisibilitySensor partialVisibility offSet={{ bottom: 50 }}>
+      {({ isVisible }: any) => (
+        <div className="w-full h-[135px] max-w-[340px] mx-auto mt-[56px] flex flex-col items-center justify-center shadow-lg shadow-gray-300 md:max-w-[231px] bg-white">
+          <div className="w-[50px] h-[50px] bg-blue-primary-light rounded-[25px] mt-[-50px] flex items-center justify-center">
+            <Image src={iconSrc} alt="icone" width={25} height={20} />
+          </div>
+          <div className='flex'>
+            <span className="font-extrabold text-[40px] text-red-primary">{plus}</span>
+            <h1 className="font-extrabold text-[40px] text-red-primary">
+              {isVisible && <CountUp end={years} />}
+            </h1>
+            <span className="font-extrabold text-[40px] text-red-primary">{text}</span>
+          </div>
+
+          <p className="font-extrabold text-sm text-gray-dark">{title}</p>
+        </div>
+      )}
+    </VisibilitySensor>
   )
 }
 
